d89529e1343916d8b1d3f48909f33b21167d4c5b
Vikas Patel <vikaspatel07071999@gmail.com>
2024-06-25 01:50:34 +0530

    june 24

---
+++ testing/js/app3.js
@@ -0,0 +1,9 @@
+// // // console.log("jhgf");
+// // let [firstName, lastName] = ['Foo', 'Bar'];
+// // console.log(firstName);
+
+// let {firstName, lastName} = {
+//     firstName: 'Foo',
+//     lastName: 'Bar'
+//   }
+//   console.log(lastName);
---
+++ testing/js/class.js
@@ -0,0 +1,11 @@
+class Car{
+    constructor(name,price,id){
+        this.name=name;
+        this.price=price;
+        this.id=id;
+    }
+
+}
+
+let tata=new Car("nexon",78000,7654);
+console.log(tata);
